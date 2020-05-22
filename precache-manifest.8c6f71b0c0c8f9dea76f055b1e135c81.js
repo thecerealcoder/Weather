898cec4ee8786dcd0ee6ca4ca0f0adcc226a079a
thecerealcoder <thecerealcoder@gmail.com>
2020-05-22 18:28:40 -0500

    Updates

---
+++ precache-manifest.8c6f71b0c0c8f9dea76f055b1e135c81.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8581980c9dc79fe2961bc93f22ea7258",
+    "url": "/flowly/index.html"
+  },
+  {
+    "revision": "e1f64f67b25a223ca720",
+    "url": "/flowly/static/css/main.f61c1f2a.chunk.css"
+  },
+  {
+    "revision": "1dfabe42e36ff39e880c",
+    "url": "/flowly/static/js/2.94e7518a.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/flowly/static/js/2.94e7518a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f64f67b25a223ca720",
+    "url": "/flowly/static/js/main.3bae0c61.chunk.js"
+  },
+  {
+    "revision": "811bfaa80acee30f99ae",
+    "url": "/flowly/static/js/runtime-main.456ef9ab.js"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/flowly/static/media/clear.7f93fbdb.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/flowly/static/media/cloudy.ad789581.svg"
+  },
+  {
+    "revision": "c8cdb21695a664b4dbffeb1fc9631510",
+    "url": "/flowly/static/media/drizzle.c8cdb216.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/flowly/static/media/rain.d235ab65.svg"
+  },
+  {
+    "revision": "67d63a14dbbef1d431f1851da197e0ae",
+    "url": "/flowly/static/media/snow.67d63a14.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/flowly/static/media/thunder.7142c78b.svg"
+  }
+]);
